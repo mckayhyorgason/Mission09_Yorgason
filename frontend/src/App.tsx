1ec8dcdea6d535ee6ee9170a5003a16d17fe1878
mckayhyorgason <mckay.yorgason21@gmail.com>
2026-03-13 12:29:26 -0500

    added team info

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import team from "./CollegeBasketballTeams.json";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Welcome() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>College Basketball teams</h1>
+      <p>Here is a list of the teams in college</p>
     </>
-  )
+  );
 }
 
-export default App
+function Team({
+  school,
+  name,
+  city,
+  state,
+}: {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}) {
+  return (
+    <>
+      <h3>{school} "{name}"</h3>
+      <h4>
+        Location: {city}, {state}
+      </h4>
+    </>
+  );
+}
+
+function TeamList() {
+  return (
+    <>
+      {
+        team.teams.map((singleTeam) => (
+          <>
+            <Team {...singleTeam} />
+            <hr/> 
+          </>
+        ))
+      }
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Welcome />
+      <br />
+      <br />
+      <TeamList />
+    </>
+  );
+}
+
+export default App;
